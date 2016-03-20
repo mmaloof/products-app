952d522dcd4574259624dbcbccd94852ad918b81
Madison Maloof <maddymaloof@gmail.com>
2016-03-20 11:12:45 -0500

    New Images Model

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160318021558) do
+ActiveRecord::Schema.define(version: 20160320145034) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "images", force: :cascade do |t|
+    t.string   "image"
+    t.integer  "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
     t.string   "description"
     t.decimal  "price",       precision: 6, scale: 2
-    t.string   "image"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "material"
